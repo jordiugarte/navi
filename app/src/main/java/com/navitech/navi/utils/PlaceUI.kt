eb87857c56ff6ba82a8c4b723ebb1f95f25239bb
jonicaconstrucciones <96605544+jonicaconstrucciones@users.noreply.github.com>
2022-02-27 18:20:44 -0400

    Home screen design

---
+++ app/src/main/java/com/navitech/navi/utils/PlaceUI.kt
@@ -0,0 +1,15 @@
+package com.navitech.navi.utils
+
+import java.sql.Time
+
+object PlaceUI {
+
+    lateinit var country: String
+    lateinit var city: String
+    lateinit var time: Time
+    lateinit var climate: String
+
+    fun initialize() {
+
+    }
+}
