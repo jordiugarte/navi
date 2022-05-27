91f8c32e7153326a7ad0093f9502048f4bd8260b
mallkusoft <mallkusoft2022@gmail.com>
2022-05-27 00:06:03 -0400

    Profile

---
+++ app/src/main/java/com/navitech/navi/data/services/GPSService.kt
@@ -0,0 +1,12 @@
+package com.navitech.navi.data.services
+
+class GPSService {
+    /*var gpsTracker: GPSTracker = GPSTracker(this)
+
+    var listCall: Call<WeatherPOJO> = service.getWeather(
+        gpsTracker.getLatitude(),
+        gpsTracker.getLongitude(),
+        "metric",
+        passwordString
+    )*/
+}
